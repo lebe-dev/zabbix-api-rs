12818a10a255b524672752336be92ae9b03e3244
Eugene Lebedev <eugene.0x90@gmail.com>
2024-01-17 13:19:43 +0300

    Improve env var names

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-WSZL_ZABBIX_API_URL=http://localhost:3080/api_jsonrpc.php
-WSZL_ZABBIX_API_USER=Admin
-WSZL_ZABBIX_API_PASSWORD=zabbix
+ZABBIX_API_URL=http://localhost:3080/api_jsonrpc.php
+ZABBIX_API_USER=Admin
+ZABBIX_API_PASSWORD=zabbix
 
 cargo test
