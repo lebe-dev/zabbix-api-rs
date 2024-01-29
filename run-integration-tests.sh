51b18012ff62dd8f962d385d8ce2184662773ad3
Eugene Lebedev <eugene.0x90@gmail.com>
2024-01-29 12:30:28 +0300

    Update run-integration-tests.sh script

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-ZABBIX_API_URL=http://localhost:3080/api_jsonrpc.php
-ZABBIX_API_USER=Admin
-ZABBIX_API_PASSWORD=zabbix
-
+ZABBIX_API_URL=http://localhost:3080/api_jsonrpc.php \
+ZABBIX_API_USER=Admin \
+ZABBIX_API_PASSWORD=zabbix \
 cargo test
