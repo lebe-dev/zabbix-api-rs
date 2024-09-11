757a68248d6a68e3d5e5f0f64e917d71d4ea4a98
Eugene Lebedev <eugene.0x90@gmail.com>
2024-09-11 13:19:56 +0300

    Add Clone trait

--- src/host/mod.rs
+++ src/host/mod.rs
@@ -4,7 +4,7 @@ pub mod create;
 pub mod get;
 
 /// API Object: https://www.zabbix.com/documentation/6.0/en/manual/api/reference/host/object
-#[derive(Deserialize,PartialEq,Debug)]
+#[derive(Deserialize,PartialEq,Clone,Debug)]
 pub struct ZabbixHost {
     #[serde(rename = "hostid")]
     pub host_id: String,
@@ -12,14 +12,14 @@ pub struct ZabbixHost {
 }
 
 // API Object: https://www.zabbix.com/documentation/6.0/en/manual/api/reference/host/object#host-tag
-#[derive(Serialize,Deserialize,PartialEq,Debug)]
+#[derive(Serialize,Deserialize,PartialEq,Clone,Debug)]
 pub struct ZabbixHostTag {
     pub tag: String,
     pub value: String
 }
 
 /// API Object: https://www.zabbix.com/documentation/6.0/en/manual/api/reference/hostinterface/object
-#[derive(Serialize,Deserialize,PartialEq,Debug)]
+#[derive(Serialize,Deserialize,PartialEq,Clone,Debug)]
 pub struct ZabbixHostInterface {
     pub r#type: u8,
 
