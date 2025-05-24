1e6b66c3d83654e9f4df264c76d8d7d7acc4c8c1
Eugene Lebedev <eugene.0x90@gmail.com>
2025-05-24 18:55:40 +0300

    Update run-example.sh

--- run-example.sh
+++ run-example.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-ZABBIX_API_URL=http://localhost:3080/api_jsonrpc.php \
-ZABBIX_API_USER=Admin \
-ZABBIX_API_PASSWORD=zabbix \
+export ZABBIX_API_URL=http://localhost:3080/api_jsonrpc.php
+export ZABBIX_API_USER=Admin
+export ZABBIX_API_PASSWORD=zabbix
+
 cargo run --example api_basics --features v7,full
+cargo run --example create_host_group_example --features v7,full
+cargo run --example create_item_example --features v7,full
+cargo run --example get_hosts_example --features v7,full
+cargo run --example get_items_example --features v7,full
